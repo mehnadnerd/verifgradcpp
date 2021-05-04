39295af90e9df8d969e8f90607334fac3d34d9ae
Brendan Sweeney <mehnadnerd@gmail.com>
2021-05-04 03:57:25 -0500

    moving the constants into primitives.h

--- include/primitives.h
+++ include/primitives.h
@@ -46,7 +46,15 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <stdbool.h>
 #include <stdint.h>
 
-extern const uint_fast16_t softposit_approxRecipSqrt0[16];
-extern const uint_fast16_t softposit_approxRecipSqrt1[16];
+const uint_fast16_t softposit_approxRecipSqrt0[16] = {
+        0xb4c9, 0xffab, 0xaa7d, 0xf11c, 0xa1c5, 0xe4c7, 0x9a43, 0xda29,
+        0x93b5, 0xd0e5, 0x8ded, 0xc8b7, 0x88c6, 0xc16d, 0x8424, 0xbae1
+};
+const uint_fast16_t softposit_approxRecipSqrt1[16] = {
+        0xa5a5, 0xea42, 0x8c21, 0xc62d, 0x788f, 0xaa7f, 0x6928, 0x94b6,
+        0x5cc7, 0x8335, 0x52a6, 0x74e2, 0x4a3e, 0x68fe, 0x432b, 0x5efd
+};
+
+
 
 #endif
