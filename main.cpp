48fd6f121339fa769a498665e220758eb948ef33
Brendan Sweeney <mehnadnerd@gmail.com>
2021-05-06 18:54:12 -0500

    new test2

--- main.cpp
+++ main.cpp
@@ -30,7 +30,10 @@ std::pair<std::vector<double>, std::vector<double> > generate_doubles(char** arg
     } else if (strcmp(args[1], "test3") == 0) {
         av = {0.25};
         bv = {1.0};
-    } else {
+    } else if (strcmp(args[1], "test4") == 0) {
+        av = {4.0};
+        bv = {1.0};
+    }else {
         // random
         std::random_device rd;
         std::mt19937 gen(rd());
